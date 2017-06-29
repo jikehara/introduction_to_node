a23d6f1108e1d02ab0f8e7f347800445b2cfb992
root <root@Naptop.localdomain>
2017-06-29 10:28:47 -0700

    Added data, modified get and post

--- index.js
+++ index.js
@@ -7,19 +7,29 @@ var app = express();
 
 console.log(makeDate(10));
 
-
-
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var instagrams = [
+  { title: "My Dog", date: makeDate(10), img: "https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Shake-shiver-and-tremble-Why-dogs-do-it.jpg?itok=yvOUgQeL" },
+  { title: "Also my dog", date: makeDate(8), img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR04cM7atojGKtFOZTukDBBqvVy13OgO3ZPrib39rtHEE_qGhdh" },
+  { title: "Not my dog", date: makeDate(7), img: "http://barkingroyalty.com/wp-content/uploads/2015/12/Beagle-puppy.jpg?x30644" },
+]
+
 app.get("/instagrams", function(req, res) {
-  res.send("Hello There!");
+  res.json({message: "Found data", data: instagrams});
 });
 
 app.post("/instagrams", function(req, res) {
-  res.send("hello from post");
+
+  var newGram = {
+    title: req.body.title,
+    img: req.body.img,
+    date: makeDate(0)};
+
+  instagrams.push(newGram);
+
+  res.json({message: "Post success", data: instagrams});
 });
 
 // this must be the last bit of code, other stuff won't run after listen.
